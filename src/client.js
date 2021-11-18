376c11fe4d9e4f7f27a453555017587d2c364706
Luis Fernando <luispeixoto@pq.uenf.br>
2021-11-18 18:04:43 -0300

    adicionando o compartilhamento de contatos

--- src/client.js
+++ src/client.js
@@ -1,11 +1,20 @@
 const xmlrpc = require('xmlrpc');
 var readlineSync = require('readline-sync');
+const fs = require('fs');
 
-var receiver = xmlrpc.createClient("http://9783-177-131-164-62.ngrok.io")
+var contactsFile= fs.readFileSync('contacts.txt', 'utf8').split('\n');
+var contacts= contactsFile.map(contact => contact.split(' '));
+
 var myServer = xmlrpc.createClient({ host: 'localhost', port: 9090 })
 
+var receiver = [myServer]
+
+function receiverContact (contact) {
+    receiver.push(xmlrpc.createClient(contact[1]))
+}
+
 function sendServer(userName, msg) {
-    [receiver,myServer].forEach((client) => {
+    receiver.forEach((client) => {
         client.methodCall('send', [userName, msg])
     })
 }
@@ -20,8 +29,26 @@ function sendMessage(userName) {
 }
 
 function Client() {
-    var userName = readlineSync.question('Nome de usuario:');
-    sendMessage(userName);    
+
+    const options = [["send", "Enviar mensagens para lista de contatos"], ["sendContacts", "Enviar lista de contatos"]]
+    
+    var OptionsSelected = readlineSync.keyInSelect(options, 'Selecione uma opcao:');
+
+    if(OptionsSelected == 0) {
+    var inputSelected = readlineSync.keyInSelect(contacts, 'Selecione um contato:');
+    var selectedContact = contacts[inputSelected];
+    
+        if (inputSelected !== -1) {
+            var userName = readlineSync.question('Nome de usuario:');
+            receiverContact(selectedContact)
+            sendMessage(userName);
+        }
+    } else {
+        myServer.methodCall('sendContacts', contacts);
+    }
+
+    
+
 }
 
 Client()
--- src/server.js
+++ src/server.js
@@ -1,12 +1,41 @@
 var xmlrpc = require('xmlrpc')
+var fs = require('fs')
  
 var server = xmlrpc.createServer({ host: 'localhost', port: 9090 })
 
+var contactsFile= fs.readFileSync('contacts.txt', 'utf8').split('\n');
+
 server.on('send', function (err, params) {
-    const userName = params[0]; 
+    const userName = params[0];
     const msg = params[1];
     console.log(`${userName}: ${msg}`)
 
     err && console.log(err) 
+})
+
+server.on('sendContacts', function (err, params) {
+    const userName = []
+    const userNameFile = []
+    const url = []
+    params.forEach(contact => {
+        userName.push(contact[0])
+        userNameFile.push(contact[0])
+    })
+
+    const diference = userName.filter(contact => !userNameFile.includes(contact))
+
+    diference.forEach(contactuser => {
+        params.forEach(contact => {
+            if (contact[0] === contactuser) {
+                url.push(contact[1])
+            }
+        })
+    })
+
+    console.log(`${diference}: ${url}`)
+
+
+
+    
 })
 console.log('Servidor rodando')
